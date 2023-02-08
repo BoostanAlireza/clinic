36354fc0c9aff4b08a802df9091f94b317492ed4
Alireza1999 Devops <boostanalireza1999@gmail.com>
2023-02-08 21:35:14 +0330

    models were registered

--- clinic_management/admin.py
+++ clinic_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(PatientPersonalInfo)
+admin.site.register(Medical_history)
+admin.site.register(TreatmentSession)
