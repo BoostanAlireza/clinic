cb1e69bc8d2d8520a813cd28f4302935574cb789
Alireza1999 Devops <boostanalireza1999@gmail.com>
2023-02-14 12:10:19 +0330

    modified

--- clinic_management/admin.py
+++ clinic_management/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(PatientPersonalInfo)
-admin.site.register(Medical_history)
+class MedicalHistoryInline(admin.TabularInline):
+    model = MedicalHistory
+
+class TreatmentSessionInline(admin.TabularInline):
+    model = TreatmentSession
+
+class PatientPersonalInfoAdmin(admin.ModelAdmin):
+    inlines = [MedicalHistoryInline, TreatmentSessionInline]
+    list_display = ('last_name', 'first_name', 'father_name', 'file_number',
+                    'identification_number', 'birth_date', 'phone', 'mobile_phone')
+    list_filter = ['last_name', 'file_number', 'identification_number']
+    search_fields = ['last_name', 'file_number', 'identification_number']
+
+
+
+admin.site.register(PatientPersonalInfo, PatientPersonalInfoAdmin)
+admin.site.register(MedicalHistory)
 admin.site.register(TreatmentSession)
